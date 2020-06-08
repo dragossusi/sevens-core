bb8c0abe85b22475ccc2fe19c6dc96c1841a5de1
Dragos Rachieru <rachierudragos97@gmail.com>
2020-06-08 20:08:04 +0300

    Added linux platform

---
+++ logger-mpp/src/iosMain/kotlin/ro/sevens/logger/IosLogger.kt
@@ -0,0 +1,42 @@
+package ro.sevens.logger
+
+/**
+ * server
+ *
+ * Copyright (C) 2020  Rachieru Dragos-Mihai
+ *
+ * server is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with server.  If not, see [License](http://www.gnu.org/licenses/) .
+ *
+ */
+open class IosLogger : TagLogger {
+    override fun i(message: String) {
+        println("I: $message")
+    }
+
+    override fun d(message: String) {
+        println("D: $message")
+    }
+
+    override fun e(message: String) {
+        println("E: $message")
+    }
+
+    override fun e(throwable: Throwable) {
+        println("E: $throwable")
+    }
+
+    override fun w(message: String) {
+        println("W: $message")
+    }
+}
